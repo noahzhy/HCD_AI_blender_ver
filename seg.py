0023f221fc849ebe25541e85be105a9d99bf5cb1
noahzhy <noahzhy@gmail.com>
2023-01-28 08:36:55 +0900

    add seg and handSeg

--- seg.py
+++ seg.py
@@ -1,30 +1,47 @@
+import os
 import bpy
 import random
 
 
 class SynthSeg():
-    def __init__(self, num, debug=False):
+    def __init__(self, num, debug=False, outpath=""):
         self.num = num
         self.debug = debug
+        self.outpath = outpath
+        # init scene
+        scene = bpy.context.scene
+        scene.use_nodes = True
+        self.nodes = scene.node_tree.nodes
+        self.nodes["File Output"].base_path = outpath
+
+    def reset(self):
+        pass
 
     def render(self):
-        # get scene
-        scene = bpy.context.scene
-        node_sets = scene.node_tree.nodes
-        scene.use_nodes = True
-
         for i in range(self.num):
             # random flip axis
-            node_sets["flip"].axis = random.choice(["X", "Y", "XY"])
+            self.nodes["flip"].axis = random.choice(["X", "Y", "XY"])
             # random angle in degrees [0, 90, 180, 270]
-            angle = random.choice([0, 90, 180, 270])
+            angle = random.randint(0, 3) * 90
             # set rotation angle
-            node_sets["angle of rotation"].outputs[0].default_value = float(angle)
-            # render and save
+            self.nodes["angle of rotation"].outputs[0].default_value = float(angle)
+            # # render and save
             if not self.debug:
-                bpy.ops.render.render(write_still=True)
-                print("rendering image: ", i)
+                self.render_diff_layer()
+
+            print("rendering folder path: ", os.path.join(self.outpath))
+
+    @staticmethod
+    def render_diff_layer(main_viewlayer='ViewLayer', part_viewlayer='ViewLayer_part'):
+        # switch to part view layer
+        bpy.context.object.modifiers["GeometryNodes"]["Input_2"] = 1
+        bpy.ops.render.render(write_still=True, layer=part_viewlayer)
+        # switch to view layer
+        bpy.context.object.modifiers["GeometryNodes"]["Input_2"] = 0
+        bpy.ops.render.render(write_still=True, layer=main_viewlayer)
 
 
 if __name__ == "__main__":
-    SynthSeg(1, debug=True).render()
+    output_path = "D:/projects/HCD_AI_blender_ver/data"
+    ss = SynthSeg(1, debug=False, outpath=output_path)
+    ss.render()
